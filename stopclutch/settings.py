dab1594d050de0747bb1b9b011a9d07e87d3efc0
Dylan Boyd <dylanjboyd@me.com>
2019-06-14 11:03:45 +1200

    Update settings.py

--- stopclutch/settings.py
+++ stopclutch/settings.py
@@ -1,4 +1,5 @@
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 # from stopclutch.secrets import *
@@ -7,7 +8,33 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEBUG = os.environ == 'DEV'
 IS_TEST = 'TRAVIS' in os.environ
 IS_PRODUCTION = not (DEBUG or IS_TEST)
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split() if IS_PRODUCTION else []
 
+if IS_TEST:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'travis_ci_test',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
+elif IS_PRODUCTION:
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+}
+elif DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
+SECRET_KEY = os.getenv('SECRET_KEY') if IS_PRODUCTION else '!*8(_j2!%+nb14fd(4r+k-*&0t3e19hxh@dr=#tvl1x^4in$sy'
 INSTALLED_APPS = [
     'races.apps.RacesConfig',
     'api.apps.ApiConfig',
@@ -25,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -101,3 +129,4 @@ REST_FRAMEWORK = {
 }
 
 SILENCED_SYSTEM_CHECKS = [None if IS_PRODUCTION else 'captcha.recaptcha_test_key_error']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
