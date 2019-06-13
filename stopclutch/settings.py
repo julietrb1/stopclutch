462bca7c4cd1c0ff2d2916b69b24223d2e133612
Dylan Boyd <dylanjboyd@me.com>
2019-06-14 11:16:00 +1200

    Update settings.py

--- stopclutch/settings.py
+++ stopclutch/settings.py
@@ -25,7 +25,7 @@ elif IS_PRODUCTION:
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
     }
-}
+
 elif DEBUG:
     DATABASES = {
         'default': {
